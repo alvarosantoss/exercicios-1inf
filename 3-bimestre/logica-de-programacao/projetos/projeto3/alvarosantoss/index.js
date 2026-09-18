ca6fd65eed12a511f4383333e075fc9350113dbf
Álvaro Luis <alvaroluissantossilva@gmail.com>
2026-09-18 01:00:56 +0000

    projeto concluido

--- 3-bimestre/logica-de-programacao/projetos/projeto3/alvarosantoss/index.js
+++ 3-bimestre/logica-de-programacao/projetos/projeto3/alvarosantoss/index.js
@@ -1,25 +1,70 @@
 const nome= "Rodrigo Nunes"
 const idade= 24
-const categoria=comum
-const ingresso= true
+const categoria="comum"
+const possuiIngresso= true
 const impedido= false
 const valorIngresso= 80
 const valorPago= 100
 
+let acessoStatus=1
+let idadeStatus=1
+let nivelAcesso=1
+let pagamentoStatus=1
+let statusPortao=1
+
 if(idade>=18){
-    console.log("Idade permitida")
+    idadeStatus="Idade permitida"
 }else{
-    console.log("Idade não permitida")
+    idadeStatus="Idade não permitida"
 }
 if(
     categoria=== "organizador" ||
     categoria=== "staff"){
-        console.log("Acesso administrativo liberado")
+        nivelAcesso="Acesso administrativo liberado"
     }else{
-        console.log("Acesso comum")
+        nivelAcesso="Acesso comum"
     }
-    if(idade>=18&&ingresso===true&&impedido!=true){
-        console.log("Acesso liberado")
+    if(idade>=18&&possuiIngresso===true&&impedido!=true){
+        acessoStatus="Acesso liberado"
     }else{
-        console.log("Acesso negado")
+        acessoStatus="Acesso negado"
     }
+    if(valorPago>=valorIngresso){
+        pagamentoStatus="Pagamento aprovado"
+    }else{
+        pagamentoStatus="Pagamento insuficiente"
+    }
+    const troco= valorPago-valorIngresso
+    if(acessoStatus==="Acesso liberado"&&pagamentoStatus==="Pagamento aprovado"){
+        statusPortao="Entrada no estádio confirmada"
+    }else{
+        statusPortao="Entrada no estádio não confirmada"
+    }
+    const resumo=`nome do torcedor:${nome}
+    categoria:${categoria}
+    nivel de acesso:${nivelAcesso}
+    valor do ingresso:R$ ${valorIngresso}
+    valor pago:R$ ${valorPago}
+    troco:R$ ${troco}
+    situação do acesso:${acessoStatus}
+    situação do pagamento:${pagamentoStatus}
+    situação final:${statusPortao}
+    `
+    console.log(resumo)
+    
+    module.exports = {
+    nome,
+    idade,
+    categoria,
+    possuiIngresso,
+    impedido,
+    valorIngresso,
+    valorPago,
+    idadeStatus,
+    nivelAcesso,
+    acessoStatus,
+    pagamentoStatus,
+    troco,
+    statusPortao,
+    resumo
+}
